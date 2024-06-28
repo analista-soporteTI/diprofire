f3773f30116a4f4632818e36385cb03898beeec2
Daniel Cordero <daniel@corderodev.com>
2024-06-28 09:54:40 -0400

    feat: add navbar links to consts

---
+++ src/consts/navbarLinks.js
@@ -0,0 +1,26 @@
+export const navbarLinks = [
+  {
+    text: 'Inicio',
+    href: '/'
+  },
+  {
+    text: 'Empresa',
+    href: '/empresa'
+  },
+  {
+    text: 'Servicios',
+    href: '/servicios'
+  },
+  {
+    text: 'Productos',
+    href: '/productos'
+  },
+  {
+    text: 'Proyectos',
+    href: '/proyectos'
+  },
+  {
+    text: 'Contacto',
+    href: '/contacto'
+  }
+]
