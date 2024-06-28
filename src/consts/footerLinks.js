129b688d29a029100605cfdb1ef8a17d2d6ea537
Daniel Cordero <daniel@corderodev.com>
2024-06-28 09:54:22 -0400

    feat: add footer links to consts

---
+++ src/consts/footerLinks.js
@@ -0,0 +1,46 @@
+export const footerLinks = [
+  {
+    title: 'Empresa',
+    links: [
+      { name: 'Representaciones', url: '/nosotros' },
+      { name: 'Quiénes somos', url: '/nosotros' },
+      { name: 'Misión y visión', url: '/nosotros' },
+      { name: 'Politica y valores', url: '/nosotros' },
+      { name: 'Clientes', url: '/nosotros' }
+    ]
+  },
+  {
+    title: 'Servicios',
+    links: [
+      { name: 'Servicios de ingeniería', url: '/servicios' },
+      { name: 'Instalaciones', url: '/servicios' },
+      { name: 'Suministro de equipos', url: '/servicios' },
+      { name: 'Servicio técnico', url: '/servicios' }
+    ]
+  },
+  {
+    title: 'Productos',
+    links: [
+      { name: 'Detección de incendio', url: '/productos' },
+      { name: 'Extinción automática', url: '/productos' },
+      { name: 'Agentes limpios', url: '/productos' },
+      { name: 'Protección pasiva', url: '/productos' },
+    ]
+  },
+  {
+    title: 'Proyectos',
+    links: [
+      { name: 'Proyectos realizados', url: '/proyectos' },
+      { name: 'Proyectos en ejecución', url: '/proyectos' }
+    ]
+  },
+  {
+    title: 'Enlaces utiles',
+    links: [
+      { name: 'Ubicación', url: '/#ubicación' },
+      { name: 'Contacto', url: '/contacto' },
+      { name: 'Preguntas frecuentes', url: '/preguntasFrecuentes' },
+      { name: 'Política de privacidad', url: '/politicaPrivacidad' }
+    ]
+  }
+]
