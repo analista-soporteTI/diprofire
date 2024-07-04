0a23adda00b753952f3ecb9ddaddbdc25c53a9a8
Daniel Cordero <daniel@corderodev.com>
2024-07-04 10:44:42 -0400

    feat: add products list for gallery products

---
+++ src/components/Products.jsx
@@ -0,0 +1,31 @@
+import { LoaderCard } from '@components/status/LoaderCard'
+import { CardProduct } from '@components/products/CardProduct.jsx'
+import { Suspense } from 'react'
+import { ErrorMessage } from '@components/status/ErrorMessage'
+
+export const Products = ({ data, error }) => {
+  if (error) return <ErrorMessage />
+  if (!data) return <LoaderCard />
+
+  return (
+    <ul className='w-full mx-auto px-4 sm:px-10 gap-10 gap-y-8 flex flex-wrap justify-center lg:justify-start'>
+      {data.map(product => (
+        <CardProduct
+          key={product.sku}
+          url={`/productos/${product.slug}/`}
+          name={product.name}
+          img={product.images[0].src}
+          alt={product.images[0].alt}
+        />
+      ))}
+    </ul>
+  )
+}
+
+export const ProductsWithSuspense = ({ data, error }) => {
+  return (
+    <Suspense fallback={<LoaderCard />}>
+      <Products data={data} error={error} />
+    </Suspense>
+  )
+}
