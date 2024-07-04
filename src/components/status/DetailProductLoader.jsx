22f21260978fbaa7c1f9839f37f93aac224ec0de
Daniel Cordero <daniel@corderodev.com>
2024-07-04 10:18:13 -0400

    feat: add detail product loader

---
+++ src/components/status/DetailProductLoader.jsx
@@ -0,0 +1,12 @@
+import '@styles/detail-product-loader.css'
+
+export const DetailProductLoader = () => {
+  return (
+    <div class='loader'>
+      <div class='image-placeholder'></div>
+      <div class='text-placeholder large'></div>
+      <div class='text-placeholder medium'></div>
+      <div class='text-placeholder small'></div>
+    </div>
+  )
+}
