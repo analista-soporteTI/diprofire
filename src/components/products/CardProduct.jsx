0916853213a355bdb25888f7eec34e24152e6619
Daniel Cordero <daniel@corderodev.com>
2024-07-04 10:39:43 -0400

    fix: change class to className in card product

--- src/components/products/CardProduct.jsx
+++ src/components/products/CardProduct.jsx
@@ -1,15 +1,15 @@
-export const CardProduct = ({ title, img, url, alt }) => {
+export const CardProduct = ({ name, img, url = "#", alt }) => {
   return (
-    <a href={url} class='group block w-[200px] rounded-lg'>
+    <a href={url} className='group block w-[200px] rounded-lg'>
       <img
         src={img}
         alt={alt}
-        class='aspect-square w-full rounded object-cover'
+        className='aspect-square w-full rounded object-cover'
       />
 
-      <div class='mt-3'>
-        <h3 class='font-medium text-sm text-gray-900 group-hover:underline group-hover:underline-offset-4'>
-          {title}
+      <div className='mt-3'>
+        <h3 className='font-medium text-base text-gray-900 group-hover:text-green-600 group-hover:underline group-hover:underline-offset-4'>
+          {name}
         </h3>
       </div>
     </a>
