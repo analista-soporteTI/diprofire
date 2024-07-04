daf3f52c135a2d24667a06af7835f7ccbcf2c5f9
Daniel Cordero <daniel@corderodev.com>
2024-07-04 10:19:30 -0400

    feat: add loader card for products

---
+++ src/components/status/LoaderCard.jsx
@@ -0,0 +1,16 @@
+import '@styles/loader-card.css'
+
+export const LoaderCard = () => {
+  return (
+    <ul className='w-full mx-auto px-4 sm:px-10 gap-10 gap-y-8 flex flex-wrap justify-center lg:justify-start'>
+      <div className='loader'></div>
+      <div className='loader'></div>
+      <div className='loader'></div>
+      <div className='loader'></div>
+      <div className='loader'></div>
+      <div className='loader'></div>
+      <div className='loader'></div>
+      <div className='loader'></div>
+    </ul>
+  )
+}
