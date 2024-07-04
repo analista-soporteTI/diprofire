0e908f2147bfbd847b82781cde7ab49ee4d166d0
Daniel Cordero <daniel@corderodev.com>
2024-07-04 14:47:45 -0400

    refactor: add local environment variables

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
+/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_WC_READ_KEY
+  readonly PUBLIC_WC_READ_PASSWORD
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
