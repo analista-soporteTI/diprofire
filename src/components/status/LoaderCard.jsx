90cf8ee3123170eb82bf22a78b1c7a4470a437d4
Daniel Cordero <daniel@corderodev.com>
2024-07-08 10:49:44 -0400

    refactor: update loader card component with small and big loaders

--- src/components/status/LoaderCard.jsx
+++ src/components/status/LoaderCard.jsx
@@ -1,16 +1,31 @@
 import '@styles/loader-card.css'
 
-export const LoaderCard = () => {
+export const LoaderCardSmall = () => {
   return (
     <ul className='w-full mx-auto px-4 sm:px-10 gap-10 gap-y-8 flex flex-wrap justify-center lg:justify-start'>
-      <div className='loader'></div>
-      <div className='loader'></div>
-      <div className='loader'></div>
-      <div className='loader'></div>
-      <div className='loader'></div>
-      <div className='loader'></div>
-      <div className='loader'></div>
-      <div className='loader'></div>
+      <div className='loader-small'></div>
+      <div className='loader-small'></div>
+      <div className='loader-small'></div>
+      <div className='loader-small'></div>
+      <div className='loader-small'></div>
+      <div className='loader-small'></div>
+      <div className='loader-small'></div>
+      <div className='loader-small'></div>
+    </ul>
+  )
+}
+
+export const LoaderCardBig = () => {
+  return (
+    <ul className='w-full px-4 gap-10 gap-y-8 flex flex-wrap justify-center'>
+      <div className='loader-big'></div>
+      <div className='loader-big'></div>
+      <div className='loader-big'></div>
+      <div className='loader-big'></div>
+      <div className='loader-big'></div>
+      <div className='loader-big'></div>
+      <div className='loader-big'></div>
+      <div className='loader-big'></div>
     </ul>
   )
 }
