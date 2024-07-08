bf998fa7fcdab0a0ff8d72a728b745e9b5fd900b
Daniel Cordero <daniel@corderodev.com>
2024-07-08 12:12:54 -0400

    refactor: optimize gallery projects with Zustand store

--- src/components/GalleryProjects.jsx
+++ src/components/GalleryProjects.jsx
@@ -1,12 +1,15 @@
+import { useEffect } from 'react'
+import useSWR from 'swr'
 import { getProjects } from '@api/apiWordpress'
 import { ProjectCard } from '@components/ProjectCard.jsx'
 import { LoaderCardBig } from '@components/status/LoaderCard'
-import { useEffect, useState } from 'react'
-import useSWR from 'swr'
+import useProjectStore from '@hooks/storeProjects'
 
 export const GalleryProjects = () => {
+  const projects = useProjectStore(state => state.projects)
+  const setProjects = useProjectStore(state => state.setProjects)
+
   const { data, error } = useSWR('projects', getProjects)
-  const [projects, setProjects] = useState(data)
 
   useEffect(() => {
     if (data) {
@@ -19,17 +22,17 @@ export const GalleryProjects = () => {
       }))
       setProjects(parsedData)
     }
-  }, [data])
+  }, [data, setProjects])
 
   return (
     <>
       {error && <p className='mx-auto'>Error al cargar los proyectos</p>}
-      {!projects && (
+      {!projects.length && !data && (
         <div className='max-w-[1400px] mx-auto'>
           <LoaderCardBig />
         </div>
       )}
-      {projects && (
+      {!!projects.length && (
         <div className='max-w-[1400px] mx-auto gap-10 px-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
           {projects.map(project => (
             <ProjectCard key={project.id} project={project} />
