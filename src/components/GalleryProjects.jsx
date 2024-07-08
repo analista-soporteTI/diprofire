f9c77a585cdb03c5c64abc1cd7048003b4c5a02f
Daniel Cordero <daniel@corderodev.com>
2024-07-08 10:55:01 -0400

    refactor: update states of gallery projects

--- src/components/GalleryProjects.jsx
+++ src/components/GalleryProjects.jsx
@@ -1,25 +1,37 @@
-import useSWR from 'swr'
-import { getProjects } from '@api/apiWordpress.js'
+import { getProjects } from '@api/apiWordpress'
 import { ProjectCard } from '@components/ProjectCard.jsx'
+import { LoaderCardBig } from '@components/status/LoaderCard'
+import { useEffect, useState } from 'react'
+import useSWR from 'swr'
 
 export const GalleryProjects = () => {
   const { data, error } = useSWR('projects', getProjects)
+  const [projects, setProjects] = useState(data)
 
-  const parsedData = data?.map(project => ({
-    id: project.id,
-    title: project.title.rendered,
-    slug: project.slug,
-    date: project.date,
-    description: project.excerpt.rendered
-  }))
+  useEffect(() => {
+    if (data) {
+      const parsedData = data.map(project => ({
+        id: project.id,
+        title: project.title.rendered,
+        slug: project.slug,
+        date: project.date,
+        description: project.excerpt.rendered
+      }))
+      setProjects(parsedData)
+    }
+  }, [data])
 
   return (
     <>
-      {error && <div>Error loading projects</div>}
-      {!data && <div>Loading projects...</div>}
-      {data && (
-        <div className='flex flex-wrap gap-10  max-w-[1200px] mx-auto px-4'>
-          {parsedData.map(project => (
+      {error && <p className='mx-auto'>Error al cargar los proyectos</p>}
+      {!projects && (
+        <div className='max-w-[1400px] mx-auto'>
+          <LoaderCardBig />
+        </div>
+      )}
+      {projects && (
+        <div className='max-w-[1400px] mx-auto gap-10 px-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+          {projects.map(project => (
             <ProjectCard key={project.id} project={project} />
           ))}
         </div>
