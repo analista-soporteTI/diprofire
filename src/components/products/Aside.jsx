dbd95e1b5eae186abb3ebe0b0c4f043f046a672a
Daniel Cordero <daniel@corderodev.com>
2024-07-08 10:58:22 -0400

    refactor: update aside handle error and styles

--- src/components/products/Aside.jsx
+++ src/components/products/Aside.jsx
@@ -1,8 +1,10 @@
 export const Aside = ({ handleCategories = '', categories, errorCategories }) => {
-  if (errorCategories) return "Error al cargar las categorías"
+  if (errorCategories) {
+    return <p>Error al cargar las categorías</p>
+  }
 
   return (
-    <aside className='hidden lg:block pt-24 pb-10 w-full min-h-screen max-w-[300px] border-r border-black/20 relative'>
+    <aside className='hidden lg:block pt-[6.5rem] pb-10 w-full min-h-screen max-w-[300px] border-r border-black/20 relative'>
       <nav className='max-w-[300px] w-full h-full px-4 fixed'>
         <h2 className='mb-4 font-semibold'>Categorías</h2>
         <ul>
