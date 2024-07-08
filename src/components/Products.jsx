eb8bfb42006c8464990bfeedb03b5b7624c7a1ee
Daniel Cordero <daniel@corderodev.com>
2024-07-08 10:51:37 -0400

    refactor: remove suspense and error handling from Products component

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,11 +1,6 @@
-import { LoaderCard } from '@components/status/LoaderCard'
 import { CardProduct } from '@components/products/CardProduct.jsx'
-import { Suspense } from 'react'
 
-export const Products = ({ data, error }) => {
-  if (error) return "Error al cargar los productos"
-  if (!data) return <LoaderCard />
-
+export const Products = ({ data }) => {
   return (
     <ul className='w-full mx-auto px-4 sm:px-10 gap-10 gap-y-8 flex flex-wrap justify-center lg:justify-start'>
       {data.map(product => (
@@ -18,13 +13,5 @@ export const Products = ({ data, error }) => {
         />
       ))}
     </ul>
-  )
-}
-
-export const ProductsWithSuspense = ({ data, error }) => {
-  return (
-    <Suspense fallback={<LoaderCard />}>
-      <Products data={data} error={error} />
-    </Suspense>
   )
 }
