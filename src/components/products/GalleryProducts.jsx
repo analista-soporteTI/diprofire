267bc6286f98c339e0aeffc008e95cd6c767adb0
Daniel Cordero <daniel@corderodev.com>
2024-07-08 10:54:09 -0400

    refactor: update states of gallery products

--- src/components/products/GalleryProducts.jsx
+++ src/components/products/GalleryProducts.jsx
@@ -1,32 +1,24 @@
 import useSWR from 'swr'
 import { getCategories, getProducts } from 'src/api/apiWoocomerce.js'
 import { Aside } from '@components/products/Aside.jsx'
-import { ProductsWithSuspense } from '@components/Products'
+import { Products } from '@components/Products'
 import { useEffect, useState } from 'react'
+import { LoaderCardSmall } from '@components/status/LoaderCard'
+import { AsideLoader } from '@components/status/AsideLoader'
 
 export const GalleryProducts = () => {
   const { data: categories, error: errorCategories } = useSWR(
     'categories',
-    getCategories,
-    {
-      suspense: true,
-      revalidateOnFocus: true,
-      dedupingInterval: 120000
-    }
-  )
-
-  const listCategories = categories.filter(
-    category => category.name !== 'Sin categorizar'
+    getCategories
   )
 
   const { data: products, error: errorProducts } = useSWR(
     'products',
-    getProducts,
-    {
-      suspense: true,
-      revalidateOnFocus: true,
-      dedupingInterval: 120000
-    }
+    getProducts
+  )
+
+  const listCategories = categories?.filter(
+    category => category.name !== 'Sin categorizar'
   )
 
   const [listProducts, setListProducts] = useState([])
@@ -42,7 +34,7 @@ export const GalleryProducts = () => {
       return setListProducts(products)
     }
 
-    let filtered = products.filter(product =>
+    let filtered = products?.filter(product =>
       product.categories.some(cat => cat.name === category)
     )
 
@@ -51,17 +43,24 @@ export const GalleryProducts = () => {
 
   return (
     <div className='flex gap-10'>
-      <Aside
-        handleCategories={handleCategories}
-        categories={listCategories}
-        errorCategories={errorCategories}
-      />
+      {!listCategories && <AsideLoader />}
+      {listCategories && (
+        <Aside
+          handleCategories={handleCategories}
+          categories={listCategories}
+          errorCategories={errorCategories}
+        />
+      )}
       <main className='pt-24 pb-10 z-10'>
         <section>
           <h1 className='mb-10 text-3xl font-bold text-center lg:text-start lg:pl-10'>
             Todos nuestros productos
           </h1>
-          <ProductsWithSuspense data={listProducts} error={errorProducts} />
+          {errorProducts && (
+            <p className='text-center'>Error al cargar los productos</p>
+          )}
+          {!products && <LoaderCardSmall />}
+          {listProducts && <Products data={listProducts} />}
         </section>
       </main>
     </div>
