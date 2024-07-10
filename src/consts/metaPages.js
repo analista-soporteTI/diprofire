efe8e97d8a1b332d921caef3e842b45ef5462d64
Daniel Cordero <daniel@corderodev.com>
2024-07-10 17:13:10 -0400

    feat: update meta pages titles and descriptions

---
+++ src/consts/metaPages.js
@@ -0,0 +1,42 @@
+export const META_PAGES = Object.freeze({
+  HOME: {
+    title: 'Diprofire - Sistemas de protección contra incendios',
+    description:
+      'En Diprofire encontrará una amplia selección de la mejor alternativa de aplicaciones de seguridad, apegándonos a las normas nacionales e internacionales.'
+  },
+  ENTERPRISE: {
+    title: 'Empresa - Diprofire - Sistemas de protección contra incendios',
+    description:
+      'Conozca más sobre nuestra empresa, nuestra misión, visión y valores que nos guían en ofrecer sistemas de protección contra incendios de la más alta calidad.'
+  },
+  SERVICES: {
+    title: 'Servicios - Diprofire - Sistemas de protección contra incendios',
+    description:
+      'Descubra los diversos servicios que ofrecemos para garantizar la seguridad y protección contra incendios en su empresa o hogar.'
+  },
+  PRODUCTS: {
+    title: 'Productos - Diprofire - Sistemas de protección contra incendios',
+    description:
+      'Explore nuestra gama de productos diseñados para proporcionar soluciones eficaces y confiables en la protección contra incendios.'
+  },
+  PROJECTS: {
+    title: 'Proyectos - Diprofire - Sistemas de protección contra incendios',
+    description:
+      'Conozca nuestros proyectos realizados, donde hemos implementado sistemas de protección contra incendios con éxito.'
+  },
+  CONTACT: {
+    title: 'Contacto - Diprofire - Sistemas de protección contra incendios',
+    description:
+      'Póngase en contacto con nosotros para más información sobre nuestros productos y servicios, o para solicitar una cotización.'
+  },
+  CART: {
+    title: 'Carrito - Diprofire - Sistemas de protección contra incendios',
+    description:
+      'Revise los productos que ha añadido a su carrito y proceda a realizar su compra con total seguridad.'
+  },
+  NOT_FOUND: {
+    title: 'Página no encontrada - Diprofire - Sistemas de protección contra incendios',
+    description:
+      'La página que busca no se encuentra. Por favor, regrese a la página de inicio o contáctenos para más información.'
+  }
+})
