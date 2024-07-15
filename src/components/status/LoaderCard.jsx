11da5bb5bb59628f927ae1549aeddda7f850ef30
Daniel Cordero <daniel@corderodev.com>
2024-07-15 10:58:15 -0400

    refactor: add new single loader card

--- src/components/status/LoaderCard.jsx
+++ src/components/status/LoaderCard.jsx
@@ -1,6 +1,14 @@
 import '@styles/loader-card.css'
 
-export const LoaderCardSmall = () => {
+export const SingleLoaderCardSmall = () => {
+  return <div className='loader-small'></div>
+}
+
+export const SingleLoaderCardBig = () => {
+  return <div className='loader-big'></div>
+}
+
+export const ListLoaderCardSmall = () => {
   return (
     <ul className='w-full mx-auto px-4 sm:px-10 gap-10 gap-y-8 flex flex-wrap justify-center lg:justify-start'>
       <div className='loader-small'></div>
@@ -15,9 +23,11 @@ export const LoaderCardSmall = () => {
   )
 }
 
-export const LoaderCardBig = () => {
+export const ListLoaderCardBig = () => {
   return (
     <ul className='w-full px-4 gap-10 gap-y-8 flex flex-wrap justify-center'>
+      <div className='loader-big'></div>
+      <div className='loader-big'></div>
       <div className='loader-big'></div>
       <div className='loader-big'></div>
       <div className='loader-big'></div>
