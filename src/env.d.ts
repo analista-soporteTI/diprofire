a7e217b860d06305ea495f6513285c948c9c1061
Daniel Cordero <daniel@corderodev.com>
2024-07-17 14:31:22 -0400

    chore: add emailJS configuration to env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,6 +4,9 @@
 interface ImportMetaEnv {
   readonly PUBLIC_WC_READ_KEY
   readonly PUBLIC_WC_READ_PASSWORD
+  readonly PUBLIC_EMAILJS_SERVICE_ID
+  readonly PUBLIC_EMAILJS_TEMPLATE_ID
+  readonly PUBLIC_EMAILJS_PUBLIC_KEY
 }
 
 interface ImportMeta {
