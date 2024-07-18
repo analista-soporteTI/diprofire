f5f0b742308dc50cc1f7273bd260c869b1a829a5
Daniel Cordero <daniel@corderodev.com>
2024-07-18 17:00:18 -0400

    refactor: update loader card small and big styles and structure

--- src/components/status/LoaderCard.jsx
+++ src/components/status/LoaderCard.jsx
@@ -1,27 +1,21 @@
 import '@styles/loader-card.css'
 
-export const SingleLoaderCardSmall = () => {
-  return <div className='loader-small'></div>
-}
-
-export const ListLoaderCardSmall = () => {
+export const LoaderCardSmall = () => {
   return (
-    <div className='w-full mx-auto px-4 sm:px-10 gap-10 gap-y-8 flex flex-wrap justify-center lg:justify-start'>
-      <div className='loader-small'></div>
-      <div className='loader-small'></div>
-      <div className='loader-small'></div>
-      <div className='loader-small'></div>
-      <div className='loader-small'></div>
-      <div className='loader-small'></div>
-      <div className='loader-small'></div>
-      <div className='loader-small'></div>
+    <div className='w-[240px] max-h-[284px] rounded-md overflow-hidden'>
+      <div className='animate-pulse flex flex-col space-y-4 p-4'>
+        <div className='h-44 bg-gray-300 rounded'></div>
+        <div className='h-4 bg-gray-300 rounded w-full my-2'></div>
+        <div className='h-4 bg-gray-300 rounded w-full my-2'></div>
+        <div className='h-4 bg-gray-300 rounded w-full my-2'></div>
+      </div>
     </div>
   )
 }
 
-export const ListLoaderCardBig = () => {
+export const LoaderCardBig = () => {
   return (
-    <div className='loader-card min-h-[140px] rounded-md max-w-sm w-full'>
+    <div className='loader-card min-h-[140px] rounded-md max-w-sm min-w-full'>
       <div className='animate-pulse flex space-x-4'>
         <div className='flex-1 py-1'>
           <div className='h-3 bg-gray-300 rounded w-1/3'></div>
