f5f1acfe08518fa8ee4ce83941782d7f3aea21a3
Daniel Cordero <daniel@corderodev.com>
2024-07-18 17:01:42 -0400

    refactor: update styles of card product

--- src/components/products/CardProduct.jsx
+++ src/components/products/CardProduct.jsx
@@ -1,14 +1,13 @@
-export const CardProduct = ({ name, img, url = "#", alt }) => {
+export const CardProduct = ({ name, img, url = '#', alt }) => {
   return (
     <a href={url} className='group block w-[200px] rounded-lg'>
       <img
         src={img}
         alt={alt}
-        className='aspect-square w-full rounded object-cover'
+        className='aspect-square w-full rounded object-cover border-2 border-zinc-200 group-hover:border-green-600 shadow-sm'
       />
-
       <div className='mt-3'>
-        <h3 className='font-medium text-base text-gray-900 group-hover:text-green-600 group-hover:underline group-hover:underline-offset-4'>
+        <h3 className='font-medium text-base text-gray-900 group-hover:text-green-600'>
           {name}
         </h3>
       </div>
