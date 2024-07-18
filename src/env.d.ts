6ee151b28802e7d08a8cddfb3f3c91e97fe3c0ff
Daniel Cordero <daniel@corderodev.com>
2024-07-18 16:58:35 -0400

    chore: add new types env for algolia

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,9 +4,13 @@
 interface ImportMetaEnv {
   readonly PUBLIC_WC_READ_KEY
   readonly PUBLIC_WC_READ_PASSWORD
+
   readonly PUBLIC_EMAILJS_SERVICE_ID
   readonly PUBLIC_EMAILJS_TEMPLATE_ID
   readonly PUBLIC_EMAILJS_PUBLIC_KEY
+
+  readonly PUBLIC_ALGOLIA_APP_ID
+  readonly PUBLIC_ALGOLIA_SEARCH_KEY
 }
 
 interface ImportMeta {
