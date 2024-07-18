39416060c6292bc199a747f4fbe88a89d1f3076f
Daniel Cordero <daniel@corderodev.com>
2024-07-18 11:54:49 -0400

    refactor: update loader card big styles and structure

--- src/components/status/LoaderCard.jsx
+++ src/components/status/LoaderCard.jsx
@@ -4,13 +4,9 @@ export const SingleLoaderCardSmall = () => {
   return <div className='loader-small'></div>
 }
 
-export const SingleLoaderCardBig = () => {
-  return <div className='loader-big'></div>
-}
-
 export const ListLoaderCardSmall = () => {
   return (
-    <ul className='w-full mx-auto px-4 sm:px-10 gap-10 gap-y-8 flex flex-wrap justify-center lg:justify-start'>
+    <div className='w-full mx-auto px-4 sm:px-10 gap-10 gap-y-8 flex flex-wrap justify-center lg:justify-start'>
       <div className='loader-small'></div>
       <div className='loader-small'></div>
       <div className='loader-small'></div>
@@ -19,23 +15,24 @@ export const ListLoaderCardSmall = () => {
       <div className='loader-small'></div>
       <div className='loader-small'></div>
       <div className='loader-small'></div>
-    </ul>
+    </div>
   )
 }
 
 export const ListLoaderCardBig = () => {
   return (
-    <ul className='w-full px-4 gap-10 gap-y-8 flex flex-wrap justify-center'>
-      <div className='loader-big'></div>
-      <div className='loader-big'></div>
-      <div className='loader-big'></div>
-      <div className='loader-big'></div>
-      <div className='loader-big'></div>
-      <div className='loader-big'></div>
-      <div className='loader-big'></div>
-      <div className='loader-big'></div>
-      <div className='loader-big'></div>
-      <div className='loader-big'></div>
-    </ul>
+    <div className='loader-card min-h-[140px] rounded-md max-w-sm w-full'>
+      <div className='animate-pulse flex space-x-4'>
+        <div className='flex-1 py-1'>
+          <div className='h-3 bg-gray-300 rounded w-1/3'></div>
+          <div className='h-4 bg-gray-300 rounded w-3/4 mb-4 mt-1'></div>
+          <div>
+            <div className='h-4 bg-gray-300 rounded'></div>
+            <div className='h-4 bg-gray-300 rounded w-5/6 my-2'></div>
+            <div className='h-4 bg-gray-300 rounded w-1/4 mt-4'></div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
