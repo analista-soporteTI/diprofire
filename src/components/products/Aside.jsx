492814eb338d7de85ea4e58b7b8202d60edf8dae
Daniel Cordero <daniel@corderodev.com>
2024-07-18 17:05:48 -0400

    feat: add search bar to product aside

--- src/components/products/Aside.jsx
+++ src/components/products/Aside.jsx
@@ -1,42 +1,63 @@
+import { SearchBar } from '@components/SearchBar'
 import { StatusMessage } from '@components/status/StatusMessage'
 
 export const Aside = ({
   handleCategories = '',
   categories,
-  errorCategories
+  errorCategories,
+  searchTerm,
+  handleSearch
 }) => {
   return (
     <div>
       <aside className='block lg:hidden pt-[6.5rem] px-4'>
         <nav>
-          <div className='relative'>
-            <h2 className='block w-full max-w-sm mx-auto mb-2 font-bold'>
-              Filtrar por
-            </h2>
-            <select
-              onChange={e => handleCategories(e.target.value)}
-              className='block w-full max-w-sm mx-auto border px-3 py-2 rounded-md bg-zinc-200 duration-200'
-            >
-              <option value=''>Todos los productos</option>
-              {categories &&
-                categories.map(category => (
-                  <option key={category.name} value={category.name}>
-                    {category.name}
-                  </option>
-                ))}
-            </select>
-            {errorCategories && (
-              <StatusMessage
-                message='Error al cargar las categorías'
-                className='w-fit'
+          <div className='flex flex-wrap gap-4 max-w-sm mx-auto'>
+            <div className='w-full'>
+              <h2 className='block w-full mb-2 font-bold'>
+                Buscar productos
+              </h2>
+              <SearchBar
+                searchTerm={searchTerm}
+                handleSearch={handleSearch}
+                id='searchProducts'
               />
-            )}
+            </div>
+            <div className='w-full'>
+              <h3 className='block w-full mb-2 font-bold'>
+                Filtrar por
+              </h3>
+              <select
+                onChange={e => handleCategories(e.target.value)}
+                className='block w-full border px-3 py-2 rounded-md bg-zinc-200 duration-200'
+              >
+                <option value=''>Todos los productos</option>
+                {categories &&
+                  categories.map(category => (
+                    <option key={category.name} value={category.name}>
+                      {category.name}
+                    </option>
+                  ))}
+              </select>
+              {errorCategories && (
+                <StatusMessage
+                  message='Error al cargar las categorías'
+                  className='w-fit'
+                />
+              )}
+            </div>
           </div>
         </nav>
       </aside>
-      <aside className='hidden lg:block pt-[6.5rem] pb-10 w-fit min-w-[300px] min-h-screen border-r border-black/20'>
+      <aside className='hidden lg:block pt-[6.5rem] pb-10 w-fit min-w-[300px] min-h-screen h-full border-r border-black/20'>
         <nav className='max-w-[300px] w-full h-full px-4'>
-          <h2 className='mb-4 font-semibold'>Categorías</h2>
+          <h2 className='mb-4 font-semibold'>Buscar productos</h2>
+          <SearchBar
+            searchTerm={searchTerm}
+            handleSearch={handleSearch}
+            id='searchProducts'
+          />
+          <h3 className='mb-4 mt-10 font-semibold'>Categorías</h3>
           <ul>
             {categories && (
               <>
