fb63dad04bcdbd8172e6b1c357bc5a6a883d129d
Daniel Cordero <daniel@corderodev.com>
2024-07-23 16:33:43 -0400

    feat: add new filters, styles and a status message in gallery products
    Added filters to search by category and brand. Also added a status message for the search and styles for change the max width of the content

--- src/components/products/GalleryProducts.jsx
+++ src/components/products/GalleryProducts.jsx
@@ -3,13 +3,15 @@ import {
   InstantSearch,
   SearchBox,
   Configure,
-  useInfiniteHits
+  useInfiniteHits,
+  RefinementList,
+  ClearRefinements
 } from 'react-instantsearch'
 import algoliasearch from 'algoliasearch/lite'
 import { CardProduct } from '@components/products/CardProduct'
 import { StatusMessage } from '@components/status/StatusMessage'
 import { STATUS } from '@consts/status'
-import "@styles/gallery-products.css"
+import '@styles/gallery-products.css'
 
 const appId = import.meta.env.PUBLIC_ALGOLIA_APP_ID
 const searchKey = import.meta.env.PUBLIC_ALGOLIA_SEARCH_KEY
@@ -28,31 +30,31 @@ const Hit = ({ hit }) => (
   />
 )
 
-const CustomInfiniteHits = (props) => {
-  const { hits, isLastPage, showMore } = useInfiniteHits(props);
-  const sentinelRef = useRef(null);
-  
+const CustomInfiniteHits = () => {
+  const { hits, isLastPage, showMore } = useInfiniteHits()
+  const sentinelRef = useRef(null)
+
   useEffect(() => {
     if (sentinelRef.current !== null) {
-      const observer = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
+      const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
           if (entry.isIntersecting && !isLastPage) {
-            showMore();
+            showMore()
           }
-        });
-      });
+        })
+      })
 
-      observer.observe(sentinelRef.current);
+      observer.observe(sentinelRef.current)
 
       return () => {
-        observer.disconnect();
-      };
+        observer.disconnect()
+      }
     }
-  }, [isLastPage, showMore]);
+  }, [isLastPage, showMore])
 
   return (
     <div className='ais-InfiniteHits'>
-      <ul className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-6'>
+      <ul className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6'>
         {hits.map(hit => (
           <li key={hit.objectID} className='flex flex-col'>
             <Hit hit={hit} />
@@ -69,9 +71,9 @@ const CustomInfiniteHits = (props) => {
       )}
       {hits.length === 0 && (
         <StatusMessage
-          type={STATUS.ERROR}
-          message='No se encontraron productos'
-          className='w-fit mx-auto mt-10'
+          type={STATUS.INFO}
+          message='No hay coincidencias con tu búsqueda'
+          className='w-fit mt-10'
         />
       )}
     </div>
@@ -80,26 +82,42 @@ const CustomInfiniteHits = (props) => {
 
 export const GalleryProducts = () => {
   return (
-    <div className='flex flex-col items-center lg:items-start gap-10 px-4 sm:px-10 max-[1024px]:pt-20'>
-      <main className='pt-10 lg:pt-24 pb-10 z-10 w-full'>
-        <h1 className='text-3xl font-bold text-start mb-4'>
-          Todos nuestros productos
-        </h1>
-        <div className='mb-6'>
-          <InstantSearch
-            indexName={indexName}
-            searchClient={searchClient}
-          >
-            <SearchBox
-              translations={{ placeholder: 'Buscar productos...' }}
-              className='w-full max-w-md mb-16'
-              placeholder='Buscar productos...'
-            />
-            <Configure />
-            <CustomInfiniteHits />
-          </InstantSearch>
-        </div>
-      </main>
-    </div>
+    <main className='pt-10 lg:pt-24 pb-10 z-10 w-full gap-10 px-4 sm:px-10 max-[1024px]:pt-20 max-w-7xl mx-auto'>
+      <h1 className='text-3xl font-bold text-start mb-6'>
+        Todos <span className='text-green-600'>nuestros productos</span>
+      </h1>
+      <div className='mb-6'>
+        <InstantSearch indexName={indexName} searchClient={searchClient}>
+          <div className='border-b border-zinc-300 mb-10 pb-10 flex flex-wrap gap-6 lg:gap-10 xl:gap-20'>
+            <div className='w-full max-w-md'>
+              <h2 className='mb-2'>Buscar productos</h2>
+              <SearchBox
+                id='searchProducts'
+                className='w-full'
+                placeholder='Nombre, sku, marca, etc...'
+              />
+              <ClearRefinements
+                excludedAttributes={[]}
+                translations={{
+                  resetButtonText: 'Limpiar filtros'
+                }}
+              />
+            </div>
+            <div className='flex flex-wrap gap-10'>
+              <div>
+                <h2>Filtrar por categorías</h2>
+                <RefinementList attribute='taxonomies.product_cat' />
+              </div>
+              <div>
+                <h2>Filtrar por marcas</h2>
+                <RefinementList attribute='taxonomies.product_tag' />
+              </div>
+            </div>
+          </div>
+          <Configure />
+          <CustomInfiniteHits />
+        </InstantSearch>
+      </div>
+    </main>
   )
 }
