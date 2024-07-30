17e9acefc652522f018a4fe6befd3e145f7dbf47
Daniel Cordero <daniel@corderodev.com>
2024-07-30 10:59:30 -0400

    refactor: update button back styles
    add new background and hover style

--- src/components/buttons/ButtonBack.jsx
+++ src/components/buttons/ButtonBack.jsx
@@ -4,7 +4,7 @@ export const ButtonBack = ({ href, style, children, ...props }) => {
   return (
     <a
       href={href}
-      className={`w-fit text-gray-600 hover:text-black flex gap-1 ${style}`}
+      className={`w-fit mb-10 pr-3 py-1 text-gray-600 hover:text-black flex gap-1 bg-transparent rounded-md hover:bg-gray-200 duration-200 ${style}`}
       {...props}
     >
       <ArrowBackIcon className='size-6' />
