1d5db9ab3e182255ae46f32d1be36f68a1895033
Daniel Cordero <daniel@corderodev.com>
2024-07-30 16:39:24 -0400

    feat: refactor product fetching with pagination in woocommerce API

--- src/api/apiWoocomerce.js
+++ src/api/apiWoocomerce.js
@@ -7,45 +7,44 @@ const Authorization = 'Basic ' + btoa(publicKey + ':' + passwordKey)
 
 const API_WC = 'https://diprofirechile.wpcomstaging.com/wp-json/wc/v3'
 
-export const FETCH_API_WC = axios.create({
+const FETCH_API_WC = axios.create({
   baseURL: API_WC,
   headers: {
     Authorization
   }
 })
 
-export const endpoints_wc = {
+const endpoints_wc = {
   PRODUCTS: `${API_WC}/products?per_page=100`,
   PRODUCTS_CATEGORIES: `${API_WC}/products/categories`,
   PRODUCTS_TAGS: `${API_WC}/products/tags`
 }
 
 export const getProducts = async () => {
-  const allProducts = [];
-  let page = 1;
-  let response;
-  
-  response = await FETCH_API_WC.get(`${endpoints_wc.PRODUCTS}&page=${page}`);
-  allProducts.push(...response.data);
-  const totalPages = parseInt(response.headers['x-wp-totalpages'], 10);
+  try {
+    let allProducts = []
+    let page = 1
+    let perPage = 100
+    let totalProducts = 0
 
-  const requests = [];
-  for (page = 2; page <= totalPages; page++) {
-    requests.push(FETCH_API_WC.get(`${endpoints_wc.PRODUCTS}&page=${page}`));
+    do {
+      const response = await FETCH_API_WC.get(endpoints_wc.PRODUCTS, {
+        params: {
+          per_page: perPage,
+          page: page
+        }
+      })
+
+      const products = response.data
+      totalProducts = parseInt(response.headers['x-wp-total'], 10)
+      allProducts = [...allProducts, ...products]
+
+      page++
+    } while (allProducts.length < totalProducts)
+
+    return allProducts
+  } catch (error) {
+    console.error('Error fetching products:', error)
+    throw error
   }
-
-  const results = await Promise.all(requests);
-  results.forEach(result => allProducts.push(...result.data));
-
-  return allProducts;
-}
-
-export const getCategories = async () => {
-  const response = await FETCH_API_WC.get(endpoints_wc.PRODUCTS_CATEGORIES)
-  return response.data
-}
-
-export const getTags = async () => {
-  const response = await FETCH_API_WC.get(endpoints_wc.PRODUCTS_TAGS)
-  return response.data
 }
