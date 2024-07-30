83e6a35b3f34de9c4fb7744edfb6cf7d413d37f1
Daniel Cordero <daniel@corderodev.com>
2024-07-30 11:06:03 -0400

    feat: add product fetching with pagination in api of woocomerce
    This commit add the fetching of all products by implementing pagination. Instead of fetching only 100 first products at once, the code now retrieves products in batches based on the total number of pages.

--- src/api/apiWoocomerce.js
+++ src/api/apiWoocomerce.js
@@ -15,14 +15,29 @@ export const FETCH_API_WC = axios.create({
 })
 
 export const endpoints_wc = {
-  PRODUCTS: `${API_WC}/products`,
+  PRODUCTS: `${API_WC}/products?per_page=100`,
   PRODUCTS_CATEGORIES: `${API_WC}/products/categories`,
   PRODUCTS_TAGS: `${API_WC}/products/tags`
 }
 
 export const getProducts = async () => {
-  const response = await FETCH_API_WC.get(endpoints_wc.PRODUCTS)
-  return response.data
+  const allProducts = [];
+  let page = 1;
+  let response;
+  
+  response = await FETCH_API_WC.get(`${endpoints_wc.PRODUCTS}&page=${page}`);
+  allProducts.push(...response.data);
+  const totalPages = parseInt(response.headers['x-wp-totalpages'], 10);
+
+  const requests = [];
+  for (page = 2; page <= totalPages; page++) {
+    requests.push(FETCH_API_WC.get(`${endpoints_wc.PRODUCTS}&page=${page}`));
+  }
+
+  const results = await Promise.all(requests);
+  results.forEach(result => allProducts.push(...result.data));
+
+  return allProducts;
 }
 
 export const getCategories = async () => {
