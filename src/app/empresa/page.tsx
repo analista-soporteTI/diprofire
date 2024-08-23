91abf2340c48fdd36202fddddd9ec3f29da81523
Daniel Cordero <daniel@corderodev.com>
2024-08-23 15:09:20 -0400

    refactor: remove logo 3M in enterprise page

--- src/app/empresa/page.tsx
+++ src/app/empresa/page.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image'
 import kidde from '@assets/brands/kidde.png'
-import logo3M from '@assets/brands/3M.png'
 import protecto from '@assets/brands/protectowire.png'
 import { History } from '@components/sections/enterprise/History'
 import { MisionVision } from '@components/sections/enterprise/MisionVision'
@@ -20,9 +19,6 @@ export default function Enterprise () {
         <div className='flex flex-wrap gap-10 items-center justify-center mx-auto [&>div>img]:max-w-[120px] sm:[&>div>img]:max-w-[200px] [&>img]:object-contain [&>img]:aspect-video [&>img]:w-full'>
           <div>
             <Image src={kidde} alt='Logo de Kidde Fire Systems' />
-          </div>
-          <div>
-            <Image src={logo3M} alt='Logo de 3M' />
           </div>
           <div>
             <Image src={protecto} alt='Logo de Protectowire FireSystems' />
