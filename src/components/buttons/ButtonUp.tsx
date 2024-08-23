05359ccef54b88c9bd46904a7bd335d6a2a72065
Daniel Cordero <daniel@corderodev.com>
2024-08-23 15:01:54 -0400

    refactor: add prop types in ButtonUp component

--- src/components/buttons/ButtonUp.tsx
+++ src/components/buttons/ButtonUp.tsx
@@ -1,11 +1,16 @@
 import { ArrowUp } from '@/icons/ArrowUp'
 import Link from 'next/link'
 
-export const ButtonUp = ({ href }: any) => {
+interface ButtonUpProps {
+  href: string
+  className?: string
+}
+
+export const ButtonUp = ({ href, className = '' }: ButtonUpProps) => {
   return (
     <Link
       href={href}
-      className='block fixed bottom-10 right-6 animate-bounce z-50 w-full h-fit max-w-[40px] group'
+      className={`block fixed bottom-10 right-6 animate-bounce z-50 w-full h-fit max-w-[40px] group ${className}`}
     >
       <ArrowUp className='size-11 p-1.5 bg-green-400 rounded-full shadow-md fill-zinc-700 group-hover:fill-white duration-200' />
     </Link>
