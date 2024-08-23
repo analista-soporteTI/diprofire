2f96c428e415c12281aed6ba4d2328cec5366e70
Daniel Cordero <daniel@corderodev.com>
2024-08-23 15:06:01 -0400

    refactor: update project page title styles

--- src/app/proyectos/page.tsx
+++ src/app/proyectos/page.tsx
@@ -6,9 +6,9 @@ export default function Projects () {
       <section className='min-h-[60vh]'>
         <div className='max-w-[1200px] mx-auto'>
           <h1 className='text-3xl sm:text-5xl font-bold px-4 mb-6'>
-            Nuestros proyectos
+            Nuestros <span className='text-green-600'>proyectos</span>
           </h1>
-          <p className='sm:text-lg px-4 mb-20 max-w-[80ch]'>
+          <p className='sm:text-lg px-4 mb-20 max-w-[80ch] opacity-90'>
             Descubre más sobre los proyectos en los que hemos participado y cómo
             hemos implementado soluciones para mejorar la seguridad contra
             incendios de nuestros clientes.
