567ef9d12ec1f51d22483b9c4313b2e02793a032
Daniel Cordero <daniel@corderodev.com>
2024-08-26 15:56:46 -0400

    refactor: update ButtonPrimary component styling
    update the styling of the ButtonPrimary component to include an active scale animation on focus and remove the unnecessary ring effect. This improves the visual feedback and user experience when interacting with the button

--- src/components/buttons/ButtonPrimary.tsx
+++ src/components/buttons/ButtonPrimary.tsx
@@ -4,7 +4,7 @@ export const ButtonPrimary = ({ href, className = '', children, ...props }: any)
   return (
     <Link
       href={href}
-      className={`block w-fit rounded bg-green-600 px-6 py-2.5 lg:py-2 my-auto font-bold text-white hover:bg-green-700 focus:outline-none focus:ring active:text-opacity-75 duration-200 ${className}`}
+      className={`block w-fit rounded bg-green-600 px-6 py-2.5 lg:py-2 my-auto font-bold text-white hover:bg-green-700 focus:outline-none active:scale-95 duration-200 ${className}`}
       {...props}
     >
       {children}
