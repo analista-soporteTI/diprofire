30f7982987942dd728bdbc71bf6e975a0531f39f
Daniel Cordero <daniel@corderodev.com>
2024-08-26 16:10:27 -0400

    refactor: update mailto functions for Gmail and Hotmail

--- src/hooks/mailto.ts
+++ src/hooks/mailto.ts
@@ -1,41 +1,81 @@
-export const mailtoDetailProduct = (product: any) => {
-  return (
-    `mailto:ventas@diprofire.cl` +
-    `?subject=Cotización%20de%20${encodeURIComponent(product.name)}` +
-    `&body=Hola,%20me%20gustaría%20cotizar.%0D%0A%0D%0A` +
-    `Producto:%20${encodeURIComponent(product.name)}%0D%0A%0D%0A` +
-    `SKU:%20${encodeURIComponent(product.sku)}%0D%0A%0D%0A` +
-    `${
-      product.model
-        ? `Modelo:%20${encodeURIComponent(product.model)}%0D%0A%0D%0A`
-        : 'Sin%20modelo%20especificado%0D%0A%0D%0A'
-    }` +
-    `${
-      product.brand
-        ? `Marca:%20${encodeURIComponent(product.brand)}%0D%0A%0D%0A`
-        : 'Sin%20marca%20especificada%0D%0A%0D%0A'
-    }` +
-    `Cantidad:%20${encodeURIComponent(product.quantity)}`
+export const mailtoDetailProductGmail = (product: any) => {
+  const subject = encodeURIComponent(`Cotización de ${product.name}`)
+  const body = encodeURIComponent(
+    `Hola, me gustaría cotizar.\n\n` +
+      `Producto: ${product.name}\n\n` +
+      `SKU: ${product.sku}\n\n` +
+      `${
+        product.model
+          ? `Modelo: ${product.model}\n\n`
+          : 'Sin modelo especificado\n\n'
+      }` +
+      `${
+        product.brand
+          ? `Marca: ${product.brand}\n\n`
+          : 'Sin marca especificada\n\n'
+      }` +
+      `Cantidad: ${product.quantity}`
   )
+
+  return `https://mail.google.com/mail/?view=cm&fs=1&to=ventas@diprofire.cl&su=${subject}&body=${body}`
 }
 
-export const mailtoCartProducts = (cart: any) => {
+export const mailtoDetailProductHotmail = (product: any) => {
+  const subject = encodeURIComponent(`Cotización de ${product.name}`)
+  const body = encodeURIComponent(
+    `Hola, me gustaría cotizar.\n\n` +
+      `Producto: ${product.name}\n\n` +
+      `SKU: ${product.sku}\n\n` +
+      `${
+        product.model
+          ? `Modelo: ${product.model}\n\n`
+          : 'Sin modelo especificado\n\n'
+      }` +
+      `${
+        product.brand
+          ? `Marca: ${product.brand}\n\n`
+          : 'Sin marca especificada\n\n'
+      }` +
+      `Cantidad: ${product.quantity}`
+  )
+
+  return `https://outlook.live.com/owa/?path=/mail/action/compose&to=ventas@diprofire.cl&subject=${subject}&body=${body}`
+}
+
+export const mailtoCartProductsGmail = (cart: any) => {
   let body = cart.reduce((acc: any, item: any) => {
     return (
-      `${acc}%0D%0A%0D%0A` +
-      `${encodeURIComponent(item.name)}%20-%20SKU:%20${encodeURIComponent(
-        item.sku
-      )}%20-%20` +
-      `${item.model ? encodeURIComponent(item.model) : 'Sin%20modelo'}%20-%20` +
-      `${item.brand ? encodeURIComponent(item.brand) : 'Sin%20marca'}%20-%20` +
-      `${encodeURIComponent(item.quantity)}%20unidad(es)`
+      `${acc}\n\n` +
+      `${item.name} - SKU: ${item.sku} - ` +
+      `${item.model ? item.model : 'Sin modelo'} - ` +
+      `${item.brand ? item.brand : 'Sin marca'} - ` +
+      `${item.quantity} unidad(es)`
     )
   }, '')
 
-  return (
-    `mailto:ventas@diprofire.cl` +
-    `?subject=Cotización%20de%20productos` +
-    `&body=Hola,%20me%20gustaría%20cotizar%20los%20siguientes%20productos:` +
-    `${body}`
+  const subject = encodeURIComponent(`Cotización de productos`)
+  const bodyText = encodeURIComponent(
+    `Hola, me gustaría cotizar los siguientes productos:${body}`
   )
+
+  return `https://mail.google.com/mail/?view=cm&fs=1&to=ventas@diprofire.cl&su=${subject}&body=${bodyText}`
+}
+
+export const mailtoCartProductsHotmail = (cart: any) => {
+  let body = cart.reduce((acc: any, item: any) => {
+    return (
+      `${acc}\n\n` +
+      `${item.name} - SKU: ${item.sku} - ` +
+      `${item.model ? item.model : 'Sin modelo'} - ` +
+      `${item.brand ? item.brand : 'Sin marca'} - ` +
+      `${item.quantity} unidad(es)`
+    )
+  }, '')
+
+  const subject = encodeURIComponent(`Cotización de productos`)
+  const bodyText = encodeURIComponent(
+    `Hola, me gustaría cotizar los siguientes productos:${body}`
+  )
+
+  return `https://outlook.live.com/owa/?path=/mail/action/compose&to=ventas@diprofire.cl&subject=${subject}&body=${bodyText}`
 }
