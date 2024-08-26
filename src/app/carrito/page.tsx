3e591e8392719fbe23c1667e2ff1864fe67c79c7
Daniel Cordero <daniel@corderodev.com>
2024-08-26 16:00:17 -0400

    fix: remove duplicate main tag on cart page

--- src/app/carrito/page.tsx
+++ src/app/carrito/page.tsx
@@ -1,9 +1,5 @@
 import { CartUI } from '@components/cart/CartUI'
 
 export default function Cart () {
-  return (
-    <main className='w-full max-w-7xl mx-auto'>
-      <CartUI />
-    </main>
-  )
+  return <CartUI />
 }
