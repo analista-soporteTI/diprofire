7fb66394dfb46186ef4990a4b3b8b1a023128e3f
Daniel Cordero <daniel@corderodev.com>
2024-08-26 15:55:43 -0400

    refactor: update ButtonCart styling of primary and secondary

--- src/components/buttons/ButtonCart.tsx
+++ src/components/buttons/ButtonCart.tsx
@@ -1,27 +1,41 @@
-import Link from "next/link"
+import Link from 'next/link'
 
 export const ButtonCartPrimary = ({
   href,
+  onClick,
+  className = '',
+  children,
+  ...props
+}: any) => {
+  return href ? (
+    <Link
+      href={href}
+      target='_blank'
+      className={`block w-fit rounded bg-green-600 px-4 py-2.5 lg:py-2 my-auto font-bold text-white hover:bg-green-700 focus:outline-none active:scale-95 duration-200 disabled:opacity-50 disabled:cursor-not-allowed ${className}`}
+      {...props}
+    >
+      {children}
+    </Link>
+  ) : (
+    <button
+      onClick={onClick}
+      className={`block w-fit rounded bg-green-600 px-6 py-2.5 lg:py-2 my-auto font-bold text-white hover:bg-green-700 focus:outline-none active:scale-95 duration-200 disabled:opacity-50 disabled:cursor-not-allowed ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  )
+}
+
+export const ButtonCartSecondary = ({
   className = '',
   children,
   ...props
 }: any) => {
   return (
-    <Link
-      href={href}
-      className={`block w-fit rounded bg-green-600 px-6 py-2.5 lg:py-2 my-auto font-bold text-white hover:bg-green-700 focus:outline-none focus:ring active:text-opacity-75 duration-200 disabled:opacity-50 disabled:cursor-not-allowed ${className}`}
-      {...props}
-    >
-      {children}
-    </Link>
-  )
-}
-
-export const ButtonCartSecondary = ({ className = '', children, ...props }: any) => {
-  return (
     <button
       type='button'
-      className={`block w-fit rounded px-6 py-2.5 lg:py-2 my-auto font-bold text-green-600 border border-green-600 hover:text-green-600 hover:bg-green-200 focus:outline-none focus:ring active:text-opacity-75 duration-200 hover:border-green-200 disabled:opacity-50 disabled:cursor-not-allowed ${className}`}
+      className={`block w-fit rounded px-4 py-2.5 lg:py-2 my-auto font-bold text-green-600 border border-green-600 hover:text-green-600 hover:bg-green-200 focus:outline-none active:scale-95 duration-200 hover:border-green-200 disabled:opacity-50 disabled:cursor-not-allowed ${className}`}
       {...props}
     >
       {children}
