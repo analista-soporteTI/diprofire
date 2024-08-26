fbe670c1143f7e5c76136694be127e712bb12454
Daniel Cordero <daniel@corderodev.com>
2024-08-26 15:56:55 -0400

    refactor: update ButtonSecondary component styling

--- src/components/buttons/ButtonSecondary.tsx
+++ src/components/buttons/ButtonSecondary.tsx
@@ -4,7 +4,7 @@ export const ButtonSecondary = ({ href, className = '', children, ...props }: an
   return (
     <Link
       href={href}
-      className={`block w-fit rounded px-6 py-2.5 lg:py-2 my-auto font-bold text-green-600 border border-green-600 hover:text-green-600 hover:bg-green-200 focus:outline-none focus:ring active:text-opacity-75 duration-200 hover:border-green-200 ${className}`}
+      className={`block w-fit rounded px-6 py-2.5 lg:py-2 my-auto font-bold text-green-600 border border-green-600 hover:text-green-700 hover:bg-green-200 focus:outline-none active:scale-95 duration-200 hover:border-green-200 ${className}`}
       {...props}
     >
       {children}
